31870970b536036e21853838a6999dc3b181aaa7
sowmya <sowmyaparthasarathy15@gmail.com>
2024-12-18 16:00:30 -0500

    Initial Commit

---
+++ Sample.java
@@ -0,0 +1,18 @@
+
+public class Sample {
+	
+	public void empId() {
+		System.out.println("employee id:3344455");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Sample s=new Sample();
+		
+		s.empId();
+	}
+
+}
