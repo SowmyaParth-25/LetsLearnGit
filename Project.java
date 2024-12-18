10d37cab6f584751b25b53532ab81b5ada73ec39
sowmya <sowmyaparthasarathy15@gmail.com>
2024-12-18 17:02:37 -0500

    final changes

--- Project.java
+++ Project.java
@@ -1,4 +1,13 @@
 
-public class Project {
+public class Project extends Sample {
+	public void empDOB() {
+		System.out.println("Employee dob is 12/3/1992");
+
+	}
+	public static void main(String[] args) {
+		Project p=new Project();
+		p.empId();
+		p.empDOB();
+	}
 
 }
--- Sample.java
+++ Sample.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Sample {
 	
-//	public void empId() {
-//		System.out.println("employee id:3344455");
-//	}
-//	
+public void empId() {
+	System.out.println("employee id:3344455");
+	}
+	
 	
 	
 	public static void main(String[] args) {
